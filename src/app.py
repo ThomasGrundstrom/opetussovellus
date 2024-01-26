6fdcfddf67f17a1039d38faea7521146e2bd5b8f
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2024-01-26 16:54:26 +0200

    update app.py

--- src/app.py
+++ src/app.py
@@ -1,8 +1,21 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, request, session
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # TODO: CHECK USERNAME AND PASSWORD
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
