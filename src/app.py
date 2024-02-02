67dad1136222a8b4045a0a9722180461fd1b774c
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2024-02-02 17:33:45 +0200

    move DATABASE_URI from app.py to .env

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///grthomas"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
 db = SQLAlchemy(app)
 app.secret_key = getenv("SECRET_KEY")
 
