d8fdbd6c4e87c1a2dc9412725f46937a6374b2be
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2024-02-04 12:40:23 +0200

    change variable name

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 app.secret_key = getenv("SECRET_KEY")
 
